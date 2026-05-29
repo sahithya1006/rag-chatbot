092d4baa367e69147dd2245b369fd5e7fcbb8b96
sahithya1006 <sahithya1006@gmail.com>
2026-05-29 21:43:04 +0530

    final working RAG chatbot clean version

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def get_answer(context, question):
 
     try:
         response = client.chat.completions.create(
-            model="llama3-8b-8192",
+            model="llama-3.1-8b-instant"
             messages=[
                 {
                     "role": "system",
